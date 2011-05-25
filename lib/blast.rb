96f9f7a883e038be7424844f17018dbc927790de
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-05-25 15:20:58 -0400

    miniblast go go go miniblast

---
+++ lib/blast.rb
@@ -0,0 +1,37 @@
+class DnaIO # iterator
+  def initialize(handle, args={})
+    @handle = handle
+  end
+  
+  def each
+    sequence, header = nil, nil
+    @handle.each do |line|
+      if line[0] == '>'
+        yield Dna.new(:name => header, :sequence => sequence) if sequence
+        sequence = ''
+        header = line[1..-1].strip
+      else
+        sequence << line.strip.tr(' ','')
+      end
+    end
+    
+  end
+
+end
+
+class Dna # nucleotide record
+  attr_accessor :name, :sequence
+  
+  def initialize(args={})
+    @name = args[:name]
+    @sequence = args[:sequence]
+  end
+  
+  def to_s
+    ">#{@name}\n#{@sequence}\n"
+  end
+  
+  def length
+    @sequence.length
+  end
+end
---
+++ readme.md
@@ -0,0 +1,5 @@
+# MiniBLAST
+
+The usual seed and extend search algorithm implemented in Ruby.
+
+    miniblast database.fasta query.fasta
