106c29fd4faeb90f212ecfff0c27c66c5b02a493
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-06-30 16:28:00 -0400

    no more tries, added kmer to args and added a require rubygems for good measure

--- lib/miniblast/miniblast.rb
+++ lib/miniblast/miniblast.rb
@@ -1,6 +1,5 @@
-require 'benchmark'
+require 'rubygems'
 require 'snappy'
-require 'trie'
 
 class Miniblast
   attr_accessor :names, :k, :hash_table, :database
@@ -10,7 +9,7 @@ class Miniblast
     @names = Hash.new
     @hash_table = Hash.new { |h, k| h[k] = Array.new }
     @database = Hash.new # save original sequences
-    @sizes = Trie.new # compressed sequence sizes
+    @sizes = Hash.new # compressed sequence sizes
   end
   
   # add a sequence to the database
